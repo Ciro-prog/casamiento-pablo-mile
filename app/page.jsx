9886a7bdeb7451cb78e3dbb4088d4f6d28a31d34
Ciro Metral <metralciro@gmail.com>
2024-02-25 18:26:22 -0300

    Eliminado archivo de diseño y actualizadas dependencias en package.json y package-lock.json.

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Tarjeta from "@/components/Tarjeta";
+import Contador from "@/components/countdown";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <Contador />
+     <Tarjeta />
+    </main>
+  );
+}
---
+++ components/Tarjeta.jsx
@@ -0,0 +1,11 @@
+
+const Tarjeta = () => {
+  return (
+    //tarjeta de invitacion de 100& de alto y 70% de ancho  
+    <div className="h-screen">
+        <img src=" /tarjeta/mile-pablo.png" alt="imagen" className="h-screen w-70" />
+    </div>
+  )
+}
+
+export default Tarjeta
