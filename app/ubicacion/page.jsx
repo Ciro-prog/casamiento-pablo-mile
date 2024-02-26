77fd79a2311ea4c036050947d5ba20dbbfc0c344
Ciro Metral <metralciro@gmail.com>
2024-02-26 11:10:56 -0300

    Agrega archivo de texto y cambia el diseño de la página principal

---
+++ app/ubicacion/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ubicacion = () => {
+  return (
+    <div>
+        <h2>Ubicacion</h2>
+        <img src=" /ubicacion/ubicacion.png" alt="imagen" className="h-50 w-50" />
+    </div>
+  )
+}
+
+export default ubicacion
